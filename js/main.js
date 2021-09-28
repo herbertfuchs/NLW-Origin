23441d5f0e815151efed54e2fa51c62f211291fe
Herbert Marcelo Cieslak Fuchs <hmcfuchs@gmail.com>
2021-09-28 18:39:01 -0300

    Fechar menu pós CLICK

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Começando a desenvolver a função para o menu mobile
 
+// Abre e fecha menu pelo ícone hamburguer e X
 const nav = document.querySelector('#header nav'); // Procurou o NAV dentro do #HEADER do HTML
 const toggle = document.querySelectorAll("nav .toggle"); // Procurou o TOGGLE dentro do NAV no HTML
 
@@ -7,5 +8,14 @@ for (const element of toggle){ // o Element está pegando o icon-menu e icon-clo
     element.addEventListener('click', function () {
         //alert('Estou funcionando!')
         nav.classList.toggle('show') // Este método TOGGLE, adiciona o show não estiver presente, e remove se estiver.
-    })
+    });
+}
+
+// quando clicar em um item do menu, esconder o menu
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+    link.addEventListener('click', function (){
+        nav.classList.remove('show') // Fecha o menu assim que clicamos em uma das opções dentro dele
+    });
 }
